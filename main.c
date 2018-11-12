99b1a2cd8053466aedfb2e9a2432fd1b2643d871
yang-seon Kim <yang-seonkim@yang-seonui-MacBookPro.local>
2018-11-12 13:37:08 +0900

    :)

--- main.c
+++ main.c
@@ -1,48 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-    int data;
-    struct node *link;
+struct node{
+    char cha;
+    struct node *next;
 };
 
-struct node *start = NULL;
+void create(struct node **, int a);
+void display(struct node *);
+char survivor(struct node **, int k);
 
-int main(void) {
-    struct node *temp, *x;
-    int count, person, mth;
+int main(){
+    struct node *head = NULL;
+    char survive;
+    int skip, person;
 
-    printf("How many people are there?: ");
+    printf("Person Number: ");
     scanf("%d", &person);
+    create(&head, person);
+    printf("The persons in circular list are:\n");
+    display(head);
+    printf("Enter the number of persons to be skipped: ");
+    scanf("%d", &skip);
+    survive = survivor(&head, skip);
+    printf("The person to survive is : %c\n", survive);
+    free(head);
 
-    printf("What number did you choose?: ");
-    scanf("%d", &mth);
-
-    for(count = 0; count < person; count++) {
-        temp = malloc(sizeof(struct node));
-        temp->data = count;
-        temp->link = NULL;
-
-        if(start == NULL) {
-            start = temp;
-        }
-        else {
-            x = start;
-            while(x->link != NULL) {
-                x = x->link;
-            }
-            x->link = temp;
-            printf("%3d ", x->data);
-        }
-    }
-   // TODO: 여기에 요세푸스 함수 넣을 것.
-    x = start;
-
-    while(x != NULL) { // 요세푸스 출력구
-        printf("%3d ", x->data);
-        x = x->link;
-    }
-    printf("\n");
     return 0;
 }
 
+char survivor(struct node **head, int k){
+    struct node *p, *q;
+    int i;
+
+    q = p = *head;
+    while (p->next != p){
+        for (i = 0; i < k - 1; i++){
+            q = p;
+            p = p->next;
+        }
+        q->next = p->next;
+        printf("%c has been killed.\n", p->cha);
+        free(p);
+        p = q->next;
+    }
+
+    *head = p;
+    return (p->cha);
+}
+
+void create (struct node **head, int a){
+    struct node *temp, *rear;
+    char ch = 'A';
+    do{
+        temp = (struct node *)malloc(sizeof(struct node));
+        temp->cha = ch;
+        temp->next = NULL;
+        if(*head == NULL){
+            *head = temp;
+        }
+
+        else{
+            rear->next = temp;
+        }
+
+        rear = temp;
+        ch++;
+        a--;
+    } while (a != 0);
+    rear->next = *head;
+}
+
+void display(struct node *head) {
+    struct node *temp;
+    temp = head;
+    printf("%3c", temp->cha);
+    temp = temp->next;
+    while (head != temp){
+        printf("%3c", temp->cha);
+        temp = temp->next;
+    }
+
+    printf("\n");
+
+}
